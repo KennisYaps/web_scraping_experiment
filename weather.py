8dbd854ffdf3ac4d99b1b84149a50face3a5279d
yappps <yapyuanxin@gmail.com>
2018-05-11 16:02:00 +0800

    added in dataframe

--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 weather_page = requests.get(
     "https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
@@ -10,4 +11,30 @@ forecast_items = seven_day_forecast.find_all(class_="tombstone-container")
 # [print(item.prettify()) for item in forecast_items]
 
 # To extract the "title" attribute from the `img` tag, Just treat the BeautifulSoup object like a dictionary, and pass in the attribute we want as a key:
-[print(item.find("img")['title']+"\n")for item in forecast_items]
+# [print(item.find("img")['title']+"\n")for item in forecast_items]
+
+# getting period_name
+
+periods = [period.get_text() for period in seven_day_forecast.select(
+    ".tombstone-container .period-name")]
+
+# getting short_description
+short_descs = [sd.get_text() for sd in seven_day_forecast.select(
+    ".tombstone-container .short-desc")]
+
+# getting temperature
+temps = [t.get_text()
+         for t in seven_day_forecast.select(".tombstone-container .temp")]
+
+# getting title attribute
+descs = [d['title']
+         for d in seven_day_forecast.select(".tombstone-container img")]
+
+weather = pd.DataFrame({
+    "period": periods,
+    "short_desc": short_descs,
+    "temp": temps,
+    "desc": descs
+})
+
+print(weather)
